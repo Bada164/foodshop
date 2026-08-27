60cc59c59c815abbd4e828067b58ba819a7e11a8
Bada164 <139805911+Bada164@users.noreply.github.com>
2026-08-27 22:56:44 +0200

    Refactor CORS policy in Program.cs
    Updated CORS policy to allow any origin in production and removed redundant CORS policy for development.

--- Program.cs
+++ Program.cs
@@ -26,10 +26,11 @@ builder.Services.AddCors((options) =>
             });
         options.AddPolicy("ProdCors", (corsBuilder) =>
             {
-                corsBuilder.WithOrigins("https://myProductionSite.com")
+                corsBuilder.WithOrigins("http://localhost:5173")
                     .AllowAnyMethod()
                     .AllowAnyHeader()
-                    .AllowCredentials();
+                    .AllowCredentials()
+                    .AllowAnyOrigin();
             });
     });
 
